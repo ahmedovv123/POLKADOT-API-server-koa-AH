3d3d54b4ea92748df054c55c33f14d9dfbc994ad
Ahmet <ahmedovv123@gmail.com>
2021-10-18 15:28:30 +0300

    initialize server

--- server.js
+++ server.js
@@ -5,13 +5,8 @@ const app = new koa();
 var router = Router();
 router.get('/hello', async function (ctx){
     ctx.body = 'hello from koa';
-
 });
 
-function* getMessage() {
-    this.body = 'Helo ';
-    console.log('sss')
-}
 
 const PORT = process.env.PORT || 8080;
 app.use(router.routes());
