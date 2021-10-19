9c703f38c15e1051d831b22376b024b13e4fab32
Ahmet <ahmedovv123@gmail.com>
2021-10-19 12:42:22 +0300

    added swagger docs

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const { Pool } = require('pg')
 const blockController = require('./src/controllers/blockController');
 const transactionController = require('./src/controllers/transactionController');
 const accountController = require('./src/controllers/accountController');
+const swagger = require('swagger2');
+const {ui, validate} = require('swagger2-koa')
+const swaggerDocument = swagger.loadDocumentSync('./openapi/openapi.yml')
+
 require('dotenv').config()
 
-
 var router = Router();
+
 
 router.get('/api/accounts/count', async function (ctx){
     const result =  await accountController.getAccountsCount(ctx);
@@ -72,6 +76,8 @@ router.get('/api/transactions/:x/:n', async function (ctx){
 
 const PORT = process.env.PORT || 8080;
 app.use(router.routes());
+app.use(ui(swaggerDocument, '/api-docs'));
+
 
 app.pool = new Pool({
     user: process.env.DB_USER || 'postgres',
